d3b7b2a1a4e30a563c8c1a372feb1ce78e2a6b99
richie-south <richard.soderman@gmail.com>
2017-11-11 20:28:27 +0100

    feat: added basic routing ad status bar color

--- pi-trainer/App.js
+++ pi-trainer/App.js
@@ -1,20 +1,43 @@
 import React from 'react'
 import {StyleSheet , View} from 'react-native'
 import {TryView} from './src/components/pages/try-view'
+import {StackNavigator} from 'react-navigation'
+import { Constants } from 'expo'
 
-export default class App extends React.Component {
+class App extends React.Component {
+  static navigationOptions = {
+    header: null,
+  }
+
   render() {
     return (
-      <TryView />
+      <View style={styles.container}>
+        <View style={styles.statusBar} />
+        <TryView />
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  statusBar: {
+    backgroundColor: "#F24949",
+    height: Constants.statusBarHeight,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundColor: '#FF7F7F',
   },
 })
+
+ export default StackNavigator({
+  Main: {
+    screen: App,
+    navigationOptions: {
+      tabBarLabel: 'Train',
+    },
+  },
+}, {})
