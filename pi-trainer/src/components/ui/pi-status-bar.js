5c81cee2617dbebcae80e94f21706073a51c65b2
richie-south <richard.soderman@gmail.com>
2017-11-11 21:11:04 +0100

    feat: status bar for error rate, precentage, digits

---
+++ pi-trainer/src/components/ui/pi-status-bar.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+import {compose, withHandlers} from 'recompose'
+import {Column, Row} from './layout'
+
+const enhance = compose(
+)
+
+const StatusProp = ({text, value}) =>
+  <View style={styles.statusPropHolder}>
+    <Text style={styles.title}>
+      {text}
+    </Text>
+    <Text style={styles.value}>
+      {value}
+    </Text>
+  </View>
+
+const StatelessPiStatusBar = ({digits, errors, precentage}) =>
+  <View>
+    <Column>
+      <Row>
+        <StatusProp
+          text='digits'
+          value={digits}
+        />
+        <StatusProp
+          text='errors'
+          value={errors}
+        />
+        <StatusProp
+          text='precentage'
+          value={precentage}
+        />
+      </Row>
+    </Column>
+  </View>
+
+const styles = StyleSheet.create({
+  statusPropHolder: {
+    marginBottom: 8,
+    height: 40,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 12,
+    color: '#444444'
+  },
+
+  value: {
+    fontSize: 16,
+    color: '#FFFFFF'
+  }
+})
+
+export const PiStatusBar = enhance(StatelessPiStatusBar)
