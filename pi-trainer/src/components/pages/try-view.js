c3c8016f7f918be93023fefc45d42b52c68c20c2
richie-south <richard.soderman@gmail.com>
2017-11-11 21:10:24 +0100

    feat: added error rate, precentage and number of entered digits

--- pi-trainer/src/components/pages/try-view.js
+++ pi-trainer/src/components/pages/try-view.js
@@ -3,19 +3,30 @@ import {StyleSheet, View, Text, ScrollView} from 'react-native'
 import {compose, withHandlers, withState, lifecycle} from 'recompose'
 import {Numberpad} from '../ui/numberpad'
 import {piNumbers} from '../../lib/number'
+import {PiStatusBar} from '../ui/pi-status-bar'
 const RCTUIManager = require('NativeModules').UIManager
 
 const isNumberCorrect = (numbers, entered, position) =>
   +numbers[position] === +entered
 
-const enhance = compose(
+const getSuccesRate = (total, errors) => {
+  return Math.floor(100 - ((errors / total) * 100 ))
+}
 
+const enhance = compose(
   withState('inputed', 'setInputed', [{ value: '3.', isCorrect: true}]),
+  withState('errors', 'setErrors', 0),
+  withState('succesRate', 'setSuccesRate', 100),
   withState('myScrollRef', 'setMyScrollRef', null),
   withHandlers({
-    addNumber: ({inputed, setInputed, myScrollRef}) => (number) => {
+    addNumber: ({inputed, setInputed, myScrollRef, errors, setErrors, setSuccesRate}) => (number) => {
       const isCorrect = isNumberCorrect(piNumbers, number, [...inputed, number].length-1)
-      setInputed([...inputed, {value: number, isCorrect}])
+      const current = [...inputed, {value: number, isCorrect}]
+      const currentErrors = errors + (isCorrect ? 0 : 1)
+
+      setSuccesRate(getSuccesRate(current.length-1, currentErrors))
+      setErrors(currentErrors)
+      setInputed(current)
 
       // auto scroll to bottom
       RCTUIManager.measure(myScrollRef.getInnerViewNode(), (...data)=>{
@@ -25,7 +36,7 @@ const enhance = compose(
   })
 )
 
-const StatelessTry = ({setMyScrollRef, inputed, addNumber}) =>
+const StatelessTry = ({setMyScrollRef, inputed, addNumber, errors, succesRate}) =>
   <View style={styles.container}>
 
     <ScrollView
@@ -47,6 +58,11 @@ const StatelessTry = ({setMyScrollRef, inputed, addNumber}) =>
     <View
       style={styles.numPadWrapper}
     >
+      <PiStatusBar
+        digits={inputed.length-1}
+        errors={errors}
+        precentage={`${succesRate}%`}
+      />
       <Numberpad
         onPress={addNumber}
       />
@@ -75,9 +91,9 @@ const styles = StyleSheet.create({
 
   inputed: {
     flex: 1,
-    marginLeft: 16,
-    marginRight: 16,
-    marginTop: 80,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 16,
   },
 
   numPadWrapper: {
