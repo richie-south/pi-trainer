8a09d4da2f068ca916898cb0e1ee8dc1e4658446
richie-south <richard.soderman@gmail.com>
2018-01-06 18:00:51 +0100

    feat: load and save from async storage

--- pi-trainer/src/lib/actions/actions.js
+++ pi-trainer/src/lib/actions/actions.js
@@ -1,13 +1,19 @@
 
-export const setRecordPi = (value) => ({
-  type: 'SET_RECORD_PI',
-  payload: value,
-})
+export const setRecordPi = (value) => {
+  AsyncStorage.setItem('@piTrainer:recordPI', value)
+  return {
+    type: 'SET_RECORD_PI',
+    payload: value,
+  }
+}
 
-export const setRecordTime = (value) => ({
-  type: 'SET_RECORD_TIME',
-  payload: value,
-})
+export const setRecordTime = (value) => {
+  AsyncStorage.setItem('@piTrainer:recordTime', value)
+  return {
+    type: 'SET_RECORD_TIME',
+    payload: value,
+  }
+}
 
 export const activateAchievements = () => ({
   type: 'ACTIVATE_ACHIEVEMENTS',
@@ -17,22 +23,34 @@ export const deactivateAchievements = () => ({
   type: 'DEACTIVATE_ACHIEVEMENTS',
 })
 
-export const increaseTextSize = (value) => ({
-  type: 'INCREASE_TEXT_SIZE',
-  payload: value,
-})
+export const increaseTextSize = (value) => {
+  AsyncStorage.setItem('@piTrainer:textSize', value)
+  return {
+    type: 'INCREASE_TEXT_SIZE',
+    payload: value,
+  }
+}
 
-export const decreaseTextSize = (value) => ({
-  type: 'DECREASE_TEXT_SIZE',
-  payload: value,
-})
+export const decreaseTextSize = (value) => {
+  AsyncStorage.setItem('@piTrainer:textSize', value)
+  return {
+    type: 'DECREASE_TEXT_SIZE',
+    payload: value,
+  }
+}
 
-export const increaseGroupSize = (value) => ({
-  type: 'INCREASE_GROUP_SIZE',
-  payload: value,
-})
+export const increaseGroupSize = (value) => {
+  AsyncStorage.setItem('@piTrainer:groupSize', value)
+  return {
+    type: 'INCREASE_GROUP_SIZE',
+    payload: value,
+  }
+}
 
-export const decreaseGroupSize = (value) => ({
-  type: 'DECREASE_GROUP_SIZE',
-  payload: value,
-})
+export const decreaseGroupSize = (value) => {
+  AsyncStorage.setItem('@piTrainer:groupSize', value)
+  return {
+    type: 'DECREASE_GROUP_SIZE',
+    payload: value,
+  }
+}
--- pi-trainer/src/lib/store.js
+++ pi-trainer/src/lib/store.js
@@ -14,13 +14,18 @@ store.dispatch(
   console.log('Success in loading async storage!')
 })
 
+// TODO: promise all..
 const loadAsyncStorage = () => async (dispatch) => {
   try {
     const time = await AsyncStorage.getItem('@piTrainer:recordTime')
+    const groupSize = await AsyncStorage.getItem('@piTrainer:groupSize')
+    const textSize = await AsyncStorage.getItem('@piTrainer:textSize')
     const pi = await AsyncStorage.getItem('@piTrainer:recordPI')
-    if (time === null || pi === null){
+    if (time === null || pi === null || groupSize === null || textSize === null){
       await AsyncStorage.setItem('@piTrainer:recordTime', 0)
       await AsyncStorage.setItem('@piTrainer:recordPI', 0)
+      await AsyncStorage.setItem('@piTrainer:textSize', 60)
+      await AsyncStorage.setItem('@piTrainer:groupSize', 8)
     } else {
       dispatch(setRecordPi(pi))
       dispatch(setRecordTime(time))
